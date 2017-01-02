235acd3808e1e02c271c9fc1f2159067d349264f
Melissa <Mnb1517@yahoo.com>
2017-01-02 13:02:33 -0600

    updates
    fixed mobile nav menu
    updated Jquery to scroll to sections
    updated parallax images

--- Portfolio Site/Portfolio.js
+++ Portfolio Site/Portfolio.js
@@ -3,8 +3,16 @@ $("#ham").click(function(e){
                $("#HamNav").slideToggle(1000); 
             });
 //This allows to click anywhere in the header to close the menu again
-$("header").click(function(e){
+$("header, .paraLayer").click(function(e){
                 $("#HamNav").slideUp(1000);                 
             });
 
-//Ask about when clicking ham img, if I can hide ham image and place mobile nav menu in its place
+
+//Scroll to sections
+$("#HamNav a").click(function(e){
+    var scrollLink = $(this).attr('href');
+    $("html, body").animate({
+       scrollTop: $(scrollLink).offset().top 
+    }, 1000);
+    
+});
